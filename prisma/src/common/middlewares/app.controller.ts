f38c874fbac5972767e626329cc7dfe7b21a49d3
Daniel <empresaprontacria@gemail.com>
2026-08-11 17:16:32 -0300

    feat: adiciona rota /tenants no AppController para listar tenants

--- prisma/src/common/middlewares/app.controller.ts
+++ prisma/src/common/middlewares/app.controller.ts
@@ -1,9 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 @Controller()
 export class AppController {
   @Get()
   getHello(): string {
     return '🚀 API Empresa Pronta rodando com sucesso no Railway!';
+  }
+
+  @Get('tenants')
+  async getTenants() {
+    return await prisma.tenant.findMany();
   }
 }
