addf17bcee1476e97d1e515a36e0b5a664b0a3a0
Daniel <empresaprontacria@gemail.com>
2026-08-11 13:46:47 -0300

    feat: cria app.module e ajusta main.ts

--- dist/main.ts
+++ dist/main.ts
@@ -4,13 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Habilita CORS para permitir conexões do frontend
   app.enableCors();
 
-  // Escuta a porta atribuída pelo Railway (ou 3000 localmente) no IP 0.0.0.0
-  const port = process.env.PORT || 3000;
-  await app.listen(port, '0.0.0.0');
+  const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
+  await app.listen(port, '0.0.0.0');
   console.log(`🚀 Aplicação rodando na porta ${port}`);
 }
 
